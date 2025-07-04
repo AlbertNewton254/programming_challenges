f404229fc11546fb4042e480c565074de5ea8767
AlbertNewton254 <miguelmochizukisilva@gmail.com>
2025-07-03 23:52:48 -0300

    partial solution pg_03

--- pg_02_minesweeper.c
+++ pg_02_minesweeper.c
@@ -1,83 +1,77 @@
-/*
- * name		:	pg_02_minesweeper.c
- * author	:	miguel
- * description	:	given a minesweeper grid, calculates adjacent mines number for each cell and maps as a new grid
- * */
+/**
+ * @file pg_02_minesweeper.c
+ * @author miguel
+ * @brief Given a minesweeper grid, calculates the number of adjacent mines for each cell
+ * and maps it into a new grid representation.
+ */
 
 #include <stdio.h>
 
-#define MAX_COLUMN 100	// max number of columns we will work with
+#define MAX_COLUMN 100	///< Max number of columns allowed
 
-/*
- * countAdjacentMines	:	function to print number of adjacent mines for a given cell in a Minesweeper grid
- * @param grid		:	2D array representing the minesweeper grid
- * @param rows		:	number of rows in the grid
- * @param cols		:	number of columns in the grid
- * @param row		:	the row index of the cell
- * @param col		:	the column index of the cell
- * @return		:	number of adjacent mines or minesweeper cell
- * */
-
-int countAdjacentMines(char grid[][MAX_COLUMN], int rows, int cols, int row, int column)
+/**
+ * @brief Counts the number of adjacent mines around a specific cell in the grid.
+ *
+ * @param grid 2D array representing the minesweeper grid.
+ *             Cells with mines are marked with '*'.
+ * @param rows Number of rows in the grid.
+ * @param cols Number of columns in the grid.
+ * @param row The row index of the cell to check.
+ * @param col The column index of the cell to check.
+ * @return Number of adjacent mines around the specified cell.
+ */
+int countAdjacentMines(char grid[][MAX_COLUMN], int rows, int cols, int row, int col)
 {
-	/* start count at 0 */
 	int count = 0;
 
-	/* we need to set all possible directions to be examinated */
 	int directions[8][2] =
-	{ {-1, -1}, {-1, 0}, {-1, 1},
-	{0, -1}, {0, 1},
-	{1, -1}, {1, 0}, {1, 1}
+	{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1},           {0, 1},
+		{1, -1},  {1, 0},  {1, 1}
 	};
 
-	/* now we start checking for each direction */
-	for (int i = 0 ; i < 8 ; i++)
+	for (int i = 0; i < 8; i++)
 	{
 		int newRow = row + directions[i][0];
-		int newCol = row + directions[i][1];
+		int newCol = col + directions[i][1]; // Corrigido: aqui era 'row + ...', virou 'col + ...'
 
-		/* validade whether we are within bounds */
 		if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols)
 		{
-			/* if the cell contains a mine, add 1 to count */
 			if (grid[newRow][newCol] == '*')
 			{
 				count++;
 			}
 		}
-	};
+	}
 
-	/* return count */
 	return count;
 }
 
-/*
- * printMinesweeperGrid		:	function to print the minesweeper grid with adjacent mines counter
- * @param grid			:	2D array representing the minesweeper grid
- * @param rows			:	number of rows in the grid
- * @param cols			:	number of columns in the grid
- * @return			:	minesweeper grid w/ adjacent mines counter
- * */
-
-int printMinesweeperGrid(int grid[][MAX_COLUMN], int rows, int cols)
+/**
+ * @brief Prints the minesweeper grid with counts of adjacent mines for non-mine cells.
+ *
+ * @param grid 2D array representing the minesweeper grid.
+ * @param rows Number of rows in the grid.
+ * @param cols Number of columns in the grid.
+ */
+void printMinesweeperGrid(char grid[][MAX_COLUMN], int rows, int cols)
 {
-	/* we will exhaust all grid cells through index numbers */
-	for (int i = 0 ; i < rows ; i++)
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 0 ; j < cols ; j ++)
+		for (int j = 0; j < cols; j++)
 		{
-			/* if the cell is a mine, print '*' */
 			if (grid[i][j] == '*')
 			{
-				printf('*');
+				printf("*");
 			}
-			
-			/* otherwise show adjacent mines counter */
 			else
 			{
 				int count = countAdjacentMines(grid, rows, cols, i, j);
 				printf("%d", count);
 			}
 		}
+		printf("\n"); // Para quebrar a linha depois de cada linha da grade
 	}
 }
+
